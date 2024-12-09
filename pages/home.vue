3d758ae425cc53d379fdedcd2d203c055a8e20a7
2000Mh16esy <ktc24a32j0002@edu.kyoto-tech.ac.jp>
2024-12-09 14:37:42 +0900

    background image done

---
+++ pages/home.vue
@@ -0,0 +1,35 @@
+<template>
+<Background
+  img-src="/undraw_Traveling_yhxq.png"
+ >
+  <b-h
+   level="1"
+   margin="5"
+   display-headings="1"
+   text-color="teal"
+  >
+   WELCOME to MYANMAR!
+  </b-h>
+ </Background>
+  <div class="home">
+    <h1>Let's start now!!</h1>
+    <p>Explore the infamous sightseeing palces and amazing places of Myanmer(Burma)</p>
+  </div>
+</template>
+
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 20px;
+  font-family: Arial, sans-serif;
+}
+
+h1 {
+  color: #42b983;
+}
+
+p {
+  font-size: 18px;
+}
+</style>
