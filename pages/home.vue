b3e809dac22185545f849ca3069013a422743282
2000Mh16esy <ktc24a32j0002@edu.kyoto-tech.ac.jp>
2024-12-16 14:25:09 +0900

    adding map

--- pages/home.vue
+++ pages/home.vue
@@ -1,75 +1,170 @@
 <template>
-<Background
-  img-src="/undraw_Traveling_yhxq.png"
- >
-  <b-h
-   level="1"
-   margin="5"
-   display-headings="1"
-   text-color="teal"
+ <b-div ratio="16x9">
+  <LMap
+   :zoom="10"
+   :center="[35.681236, 139.767125]"
+   :use-global-leaflet="false"
   >
-   WELCOME to MYANMAR!
-  </b-h>
- </Background>
+   <LTileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    layer-type="base"
+    name="OpenStreetMap"
+   />
+  </LMap>
+ </b-div>
   <div class="home">
     <h1>Let's start now!!</h1>
     <p>Explore the infamous sightseeing palces and amazing places of Myanmer(Burma)</p>
   </div>
-  <table>
-  <caption>
-    Alien football stars
-  </caption>
-  <tr>
-    <th scope="col">City name</th>
-    <th scope="col">places</th>
-    <th scope="col">see more</th>
-  </tr>
-  <tr>
-    <th scope="row">TR-7</th>
-    <td>7</td>
-    <td>4,569</td>
-  </tr>
-  <tr>
-    <th scope="row">Khiresh Odo</th>
-    <td>7</td>
-    <td>7,223</td>
-  </tr>
-  <tr>
-    <th scope="row">Mia Oolong</th>
-    <td>9</td>
-    <td>6,219</td>
-  </tr>
-    <tr>
-    <th scope="col">City name</th>
-    <th scope="col">places</th>
-    <th scope="col">see more</th>
-  </tr>
-  <tr>
-    <th scope="row">TR-7</th>
-    <td>7</td>
-    <td>4,569</td>
-  </tr>
-  <tr>
-    <th scope="row">Khiresh Odo</th>
-    <td>7</td>
-    <td>7,223</td>
-  </tr>
-  <tr>
-    <th scope="row">Mia Oolong</th>
-    <td>9</td>
-    <td>6,219</td>
-  </tr>
-    <tr>
-    <th scope="col">City name</th>
-    <th scope="col">places</th>
-    <th scope="col">see more</th>
-  </tr>
-  <tr>
-    <th scope="row">TR-7</th>
-    <td>7</td>
-    <td>4,569</td>
-  </tr>
-</table>
+
+<b-table hover>
+  <b-thead>
+   <b-tr>
+    <b-th scope="col">
+     No.
+    </b-th>
+    <b-th scope="col">
+     Place
+    </b-th>
+    <b-th scope="col">
+    City
+    </b-th>
+    <b-th scope="col">
+     About
+    </b-th>
+   </b-tr>
+  </b-thead>
+
+  <b-tbody>
+
+   <b-tr>
+    <b-th scope="row">
+     1
+    </b-th>
+    <b-td>Sule Pagoda</b-td>
+    <b-td>Yangon</b-td>
+    <b-td>@mdo</b-td>
+   </b-tr>
+
+   <b-tr>
+    <b-th scope="row">
+     2
+    </b-th>
+    <b-td>Mount Popa</b-td>
+    <b-td>Pegu</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>
+
+    <b-tr>
+     <b-th scope="row">
+     3
+    </b-th>
+    <b-td>Shwedagon Pagoda</b-td>
+    <b-td>Yangon</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>
+
+
+    <b-tr>
+     <b-th scope="row">
+     4
+    </b-th>
+    <b-td>Bagan Golden Palace</b-td>
+    <b-td>Bagan</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>
+
+
+    <b-tr>
+     <b-th scope="row">
+     5
+    </b-th>
+    <b-td>Inle Floating Gardens</b-td>
+    <b-td>Inle</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>
+
+
+    <b-tr>
+     <b-th scope="row">
+     6
+    </b-th>
+    <b-td>Mwe Taw Kakku Pagodas</b-td>
+    <b-td>Inle</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>
+
+
+    <b-tr>
+     <b-th scope="row">
+     7
+    </b-th>
+    <b-td>Mandalay Palace</b-td>
+    <b-td>Mandalay</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>
+
+
+    <b-tr>
+     <b-th scope="row">
+     8
+    </b-th>
+    <b-td>Macleod Island</b-td>
+    <b-td>Mergui</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>
+
+
+    <b-tr>
+     <b-th scope="row">
+     9
+    </b-th>
+    <b-td>Mingun Temple</b-td>
+    <b-td>Sagaing</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>
+
+
+    <b-tr>
+     <b-th scope="row">
+     10
+    </b-th>
+    <b-td>Htilominlo Temple</b-td>
+    <b-td>Bagan</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>
+
+
+    <b-tr>
+     <b-th scope="row">
+     11
+    </b-th>
+    <b-td>Ananda Temple</b-td>
+    <b-td>Bagan</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>
+
+
+    <b-tr>
+     <b-th scope="row">
+     12
+    </b-th>
+    <b-td>Dhammayangyi Temple</b-td>
+    <b-td>Bagan</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>
+
+
+    <b-tr>
+     <b-th scope="row">
+     13
+    </b-th>
+    <b-td>Shwezigon Pagoda</b-td>
+    <b-td>Bagan</b-td>
+    <b-td>@fat</b-td>
+   </b-tr>  
+  </b-tbody>
+ </b-table>
 </template>
 
 
@@ -121,5 +216,12 @@ table {
 caption {
   caption-side: bottom;
   padding: 10px;
+}
+
+.leaflet-pane{
+  z-index: 0 !important;
+}
+.leaflet-bottom, .leaflet-top {
+ z-index: 0 !important;
 }
 </style>
