952ae076e76df2082fac1b6b9dbf7a105d21c9bd
2000Mh16esy <ktc24a32j0002@edu.kyoto-tech.ac.jp>
2024-12-23 15:03:55 +0900

    page 01

---
+++ pages/page_1.vue
@@ -0,0 +1,43 @@
+<template>
+ <b-img
+  src="undraw_Traveling_yhxq.png"
+  fluid
+  relative-width="100"
+  alt="Responsive image"
+ />
+
+  <b-img
+  src="undraw_Traveling_yhxq.png"
+  fluid
+  relative-width="100"
+  alt="Responsive image"
+ />
+
+  <b-img
+  src="undraw_Traveling_yhxq.png"
+  fluid
+  relative-width="100"
+  alt="Responsive image"
+ />
+
+  <b-img
+  src="undraw_Traveling_yhxq.png"
+  fluid
+  relative-width="100"
+  alt="Responsive image"
+ />
+
+  <b-img
+  src="undraw_Traveling_yhxq.png"
+  fluid
+  relative-width="100"
+  alt="Responsive image"
+ />
+
+  <b-img
+  src="undraw_Traveling_yhxq.png"
+  fluid
+  relative-width="100"
+  alt="Responsive image"
+ />
+</template>
