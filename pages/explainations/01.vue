f8ebfce47ddb672368b836a1efb50459ff0baa49
2000Mh16esy <ktc24a32j0002@edu.kyoto-tech.ac.jp>
2025-01-20 18:06:24 +0900

    page 01

--- pages/explainations/01.vue
+++ pages/explainations/01.vue
@@ -1,5 +1,12 @@
 <template>
- <b-img
+<div class="">
+ <h1>Sule Pagoda</h1> 
+ <p>
+Located in the heart of Yangon, the Sule Pagoda is a Burmese stupa believed to be built during the time of the Buddha, about 2,600 years ago. It is surrounded by various structures of historical, business, and cultural significance and is visited by devotees from across the world.
+
+This is one of the most ancient places to visit in Myanmar and deserves a visit for its religious importance, beautiful architecture, and holy charm.)</p>
+</div>
+<b-img
   src="sule.jpg"
   fluid
   relative-width="100"
