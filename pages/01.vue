1d8c2a41ceecda1544949571d1856a1d81974eb1
2000Mh16esy <ktc24a32j0002@edu.kyoto-tech.ac.jp>
2025-01-22 16:23:13 +0900

    link fix

--- pages/01.vue
+++ pages/01.vue
@@ -1,10 +1,6 @@
 <template>
 <div>
  <h1>Sule Pagoda</h1> 
- <p>
-Located in the heart of Yangon, the Sule Pagoda is a Burmese stupa believed to be built during the time of the Buddha, about 2,600 years ago. It is surrounded by various structures of historical, business, and cultural significance and is visited by devotees from across the world.
-
-This is one of the most ancient places to visit in Myanmar and deserves a visit for its religious importance, beautiful architecture, and holy charm.</p>
 </div>
 
  <Card>
@@ -15,11 +11,12 @@ This is one of the most ancient places to visit in Myanmar and deserves a visit 
   <CardBody>
    <CardTitle>Night view of Sule Pagoda</CardTitle>
    <CardText>
-    Some quick example text to build on the card title and make up the bulk
-    of the card's content.
+    Located in the heart of Yangon, the Sule Pagoda is a Burmese stupa believed to be built during the time of the Buddha, about 2,600 years ago. It is surrounded by various structures of historical, business, and cultural significance and is visited by devotees from across the world.
+
+This is one of the most ancient places to visit in Myanmar and deserves a visit for its religious importance, beautiful architecture, and holy charm.
    </CardText>
    <Anchor
-    to="home.vue"
+    to="/home/2000Mh16esy/MT-02/pages/home.vue"
     button
     color="primary"
    >
